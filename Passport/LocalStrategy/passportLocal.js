de70b280fde70e15c8ca2d345b1511cc64f50e3e
Donhyeong Lee <dhm_w@icloud.com>
2023-06-09 13:30:48 +0900

    passport 작성 중

---
+++ Passport/LocalStrategy/passportLocal.js
@@ -0,0 +1,42 @@
+// ======================================================================================== [Import Libaray]
+const passport = require('passport'); // passport 임포트
+const LocalStrategy = require('passport-local').Strategy; // passport 로컬 인증 전략 적용
+const session = require('express-session'); // 세션 라이브러리
+
+function passportLocal(app){
+    const expireTimeMinutes=process.env.passport_session_expire_time_minutes
+    app.use(session({secret : process.env.passport_secret_code, resave : false, saveUninitialized: false, cookie: { maxAge : expireTimeMinutes*60000 }, rolling:true}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+
+    app.post('/local-login', passport.authenticate('local', {successRedirect :"/",failureRedirect : '/local-login', failureFlash : true}));
+
+    passport.use(new LocalStrategy({
+      usernameField: 'user_account', // form에서 전달받은 값 중 username으로 사용할 html div의 id 값
+      passwordField: 'user_pw', // form에서 전달받은 값 중 password로 사용할 html div의 id 값
+      session: true,
+      passReqToCallback: false,
+    }, function (userID, userPW, done) { // 첫번째 인자는 위에서 지정한 usernameField 값, 두번째 인자는 passwordField 값
+      if(userID==='a'){
+        console.log(1)
+        return done(null,userID) // 두번째 인자가 TRUE이면 세션 발급,
+      }
+      else{
+        console.log(2)
+        return done(null, false, {message:"fail"})
+      }  
+    }));
+    
+    passport.serializeUser(function (userID, done) {
+      console.log("-------")
+      console.log(userID)
+      done(null,userID)
+      console.log("Session was created.")
+    });
+    
+    passport.deserializeUser(function (user_id, done) {
+      console.log(3)
+    });
+}
+module.exports = passportLocal;
--- server.js
+++ server.js
@@ -16,10 +16,9 @@ require('dotenv').config({ path:'./secrets/.env'})
 //================================================================================ [공통] react router 관련 라이브러리 import
 const path = require('path');
 
-//================================================================================ [공통] passport 라이브러리 import
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+
+
+
 
 //================================================================================ [공통] body-parser 라이브러리 import
 const bodyParser= require('body-parser')
@@ -58,11 +57,7 @@ app.use(express.urlencoded({limit: '10mb', extended: true}))
 //================================================================================ [공통 미들웨어] connect-flash
 app.use(flash())
 
-//================================================================================ [공통 미들웨어] passport
-const expireTimeMinutes=10
-app.use(session({secret : process.env.passport_secret_code, resave : false, saveUninitialized: false, cookie: { maxAge : expireTimeMinutes*60000 }, rolling:true})); //cookie: { maxAge : 60000 } 제외함
-app.use(passport.initialize());
-app.use(passport.session());
+
 //================================================================================ [공통 미들웨어] react router 관련
 app.use(express.static(path.join(__dirname, process.env.react_build_path)));
 
@@ -74,13 +69,14 @@ app.listen(8080, function() {
 //================================================================================ https 의존성으로 certificate와 private key로 새로운 서버를 시작
 //https.createServer(options, app).listen(process.env.PORT, () => {console.log('HTTPS server started on port '+ process.env.PORT)});
 
+const passportLocal = require('./Passport/LocalStrategy/passportLocal');
+passportLocal(app);
 
+//================================================================================ [공통 기능] 모든 route를 react SPA로 연결 (이 코드는 맨 아래 있어야함)
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, process.env.react_build_path+'index.html'));
+});
 
-  //================================================================================ [공통 기능] 모든 route를 react SPA로 연결 (이 코드는 맨 아래 있어야함)
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, process.env.react_build_path+'index.html'));
-  });
-
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, process.env.react_build_path+'index.html'));
-  });
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, process.env.react_build_path+'index.html'));
+});
